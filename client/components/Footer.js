923ffc99ad94dc56ef5868529c5440bc8bbd1c8e
TheFourthLion <the00110100lion@gmail.com>
2023-05-14 17:02:56 -0700

    add footer component

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -1,10 +1,33 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="container">
-        <h1 className="content-header">Footer</h1>
-      </div>
+      <Container>
+        <Row className="footer-links">
+          <Col className="footer-nav">
+            <ul className="footer-links">
+              <li className="">
+                <Link href="/">BMT</Link>
+              </li>
+
+              <li className="">
+                <Link href="/Stats">stats</Link>
+              </li>
+              <li className="">
+                <Link href="/Daily">daily</Link>
+              </li>
+
+              <li className="">
+                <Link href="/Weekly">weekly</Link>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
