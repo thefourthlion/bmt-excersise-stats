5c6d8b0be1cfec9bbac440c87f0f36a5e461946a
TheFourthLion <the00110100lion@gmail.com>
2023-05-14 19:46:25 -0700

    deploy configs

--- client/server.js
+++ client/server.js
@@ -3,7 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = process.env.PORT || 3013;
+const port = process.env.PORT || 3020;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 app.prepare().then(() => {
